4832c4a17009a4e0675b2c61f3e94c1b5af10f5b
Hariharan148 <harirevorhustle@gmail.com>
2022-12-04 22:07:38 +0530

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+
+)
+
+type Problem struct{
+	q string
+	a string
+}
+
+func problemPuller(file string)([]string, error){
+
+
+}
+
+
+func problemParser([]string)[]Problem{
+
+}
+
+func main(){
+
+}
